b1b8ed5eb02579db6699f78a6c238fe59f0d2a3f
elumbantoruan <edison.lumbantoruan@gmail.com>
2018-09-07 22:21:14 -0700

    add module1

---
+++ module1/go.mod
@@ -0,0 +1,1 @@
+module github.com/elumbantoruan/testmod/module1
---
+++ module1/greet.go
@@ -0,0 +1,8 @@
+package module1
+
+import "fmt"
+
+// Greeting .
+func Greeting(s string) string {
+	return fmt.Sprintf("Hello %s from module1", s)
+}
